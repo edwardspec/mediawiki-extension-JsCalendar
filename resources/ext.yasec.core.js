f51bd38fe86c578ca6b70b82133300a0dbd3fc22
Steffen Beyer <steffen@beyer.io>
2014-02-08 03:54:33 +0100

    make sure calendarData is defined

--- resources/ext.yasec.core.js
+++ resources/ext.yasec.core.js
@@ -1,6 +1,8 @@
-for (var i = 0; i < window.eventCalendarData.length; i++) {
-    $("#eventcalendar-" + (i + 1)).fullCalendar({
-        aspectRatio: window.eventCalendarAspectRatio[i],
-        events: window.eventCalendarData[i]
-    });
+if (window.eventCalendarData !== undefined) {
+    for (var i = 0; i < window.eventCalendarData.length; i++) {
+        $("#eventcalendar-" + (i + 1)).fullCalendar({
+            aspectRatio: window.eventCalendarAspectRatio[i],
+            events: window.eventCalendarData[i]
+        });
+    }
 }
