c1abfd54e5d5ac01077b7860822949375e628a72
Steffen Beyer <steffen@beyer.io>
2014-02-09 14:58:59 +0100

    rerender calendar after timeout, in case display is garbled

--- resources/ext.yasec.core.js
+++ resources/ext.yasec.core.js
@@ -1,8 +1,17 @@
-if (window.eventCalendarData !== undefined) {
-    for (var i = 0; i < window.eventCalendarData.length; i++) {
-        $("#eventcalendar-" + (i + 1)).fullCalendar({
-            aspectRatio: window.eventCalendarAspectRatio[i],
-            events: window.eventCalendarData[i]
-        });
+jQuery( document ).ready( function( $ ) {
+    if ( window.eventCalendarData !== undefined ) {
+        for ( var i = 0; i < window.eventCalendarData.length; i++ ) {
+            $( "#eventcalendar-" + ( i + 1 )).fullCalendar({
+                aspectRatio: window.eventCalendarAspectRatio[i],
+                events: window.eventCalendarData[i]
+            });
+
+            // FIXME sometimes init is called too early, it seems, so rerender to be sure
+            ( function( i ) {
+                window.setTimeout( function() { 
+                    $( "#eventcalendar-" + ( i + 1 )).fullCalendar( 'render' );
+                }, 2500 );
+            })( i );
+        }
     }
-}
+});
