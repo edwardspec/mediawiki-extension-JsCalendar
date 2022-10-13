96acdced889fd777441417eaabc0133e0e8526a1
Edward Chernenko <edwardspec@gmail.com>
2022-10-14 01:00:05 +0300

    (cosmetic) Fix space/tab indentation in [ext.yasec.core.js]

--- resources/ext.yasec.core.js
+++ resources/ext.yasec.core.js
@@ -1,26 +1,26 @@
 /* This script feeds event data to FullCalendar v2. */
 
 jQuery( document ).ready( function( $ ) {
-    if ( typeof window.eventCalendarData === 'object' ) {
-        for ( var i = 0; i < window.eventCalendarData.length; i++ ) {
-            // render calendar
-            $( "#eventcalendar-" + ( i + 1 )).fullCalendar( {
-                aspectRatio: window.eventCalendarAspectRatio[i],
-                events: window.eventCalendarData[i],
-		eventRender: function( eventObj, $el ) {
-			// Allow HTML in event titles.
-			$el.html( $el.text() );
-		},
-		contentHeight: 'auto',
-		lang: mw.config.get( 'wgUserLanguage' )
-            } );
+	if ( typeof window.eventCalendarData === 'object' ) {
+		for ( var i = 0; i < window.eventCalendarData.length; i++ ) {
+			// render calendar
+			$( "#eventcalendar-" + ( i + 1 )).fullCalendar( {
+				aspectRatio: window.eventCalendarAspectRatio[i],
+				events: window.eventCalendarData[i],
+				eventRender: function( eventObj, $el ) {
+					// Allow HTML in event titles.
+					$el.html( $el.text() );
+				},
+				contentHeight: 'auto',
+				lang: mw.config.get( 'wgUserLanguage' )
+			} );
 
-            // FIXME sometimes init is called too early, it seems, so rerender to be sure
-            ( function( i ) {
-                window.setTimeout( function() {
-                    $( "#eventcalendar-" + ( i + 1 )).fullCalendar( 'render' );
-                }, 0 );
-            })( i );
-        }
-    }
+			// FIXME sometimes init is called too early, it seems, so rerender to be sure
+			( function( i ) {
+				window.setTimeout( function() {
+					$( "#eventcalendar-" + ( i + 1 )).fullCalendar( 'render' );
+				}, 0 );
+			})( i );
+		}
+	}
 });
